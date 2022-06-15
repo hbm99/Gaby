80e266fd0cc8b75c60b0b7ccbbc740c07ac50532
hbm99 <you@example.com>
2022-06-14 22:00:31 -0400

    Working on mac os

--- Gaby/Shared/Utils/HomeAddress.cs
+++ Gaby/Shared/Utils/HomeAddress.cs
@@ -9,26 +9,6 @@ namespace Gaby.Shared.Utils
 	
     public class HomeAddress
     {
-
-		/// <summary>
-		/// Creates a new address.
-		/// </summary>
-		/// <param name="main_st"></param>
-		/// <param name="secondary_st_1"></param>
-		/// <param name="secondary_st_2"></param>
-		/// <param name="house_number"></param>
-		/// <param name="community"></param>
-		/// <param name="municipality"></param>
-		//public HomeAddress(string main_st, string secondary_st_1, string secondary_st_2, int house_number, string community, string municipality)
-		//{
-		//	this.MainSt = main_st;
-		//	this.SecondarySt1 = secondary_st_1;
-		//	this.SecondarySt2 = secondary_st_2;
-		//	this.HouseNumber = house_number;
-		//	this.Community = community;
-		//	this.Municipality = municipality;
-		//}
-
 		/// <summary>
 		/// Returns the main street.
 		/// </summary>
